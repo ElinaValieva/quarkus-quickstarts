4b8bfd9fd47df21ed9afdcf529a6d5fefd8c9f86
ElinaValieva <veaufa@mail.ru>
2020-04-16 11:50:54 +0300

    Fix: remove unused

--- src/main/java/com/quarkus/controller/BlogController.java
+++ src/main/java/com/quarkus/controller/BlogController.java
@@ -2,6 +2,7 @@ package com.quarkus.controller;
 
 import com.quarkus.model.Post;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,18 +12,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/blog")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class BlogController {
 
     @GET
     @Path("/posts/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getPosts(@PathParam("id") Integer id) {
         return Response.ok("set posts with id: " + id).build();
     }
 
     @POST
     @Path("/post")
-    @Produces(MediaType.APPLICATION_JSON)
     public Response createPost(Post post) {
         return Response.ok(post).build();
     }
