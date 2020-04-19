0375aaea5770469330abfab84ee11c1fcb1c55f2
ElinaValieva <veaufa@mail.ru>
2020-04-19 16:18:23 +0300

    Feat: login component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <SearchComponent/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchComponent from "./components/SearchComponent";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        SearchComponent
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <div class="input-field">
+                <img src="../assets/images/man-user.svg"/>
+                <input class="form-control" type="text" placeholder="username" v-model="username" required/>
+            </div>
+            <div class="input-field">
+                <img src="../assets/images/lock.svg"/>
+                <input class="form-control" type="password" placeholder="password" v-model="password" required/>
+            </div>
+            <button class="btn btn-success">LOGIN</button>
+            <a href="/registration">Sing up?</a>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginComponent",
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login: function () {
+        }
+    }
+}
+</script>
+
+<style>
+    body {
+        background: #28a745;
+    }
+
+    .input-field {
+        margin-top: 5%;
+        padding: 3%;
+        border-bottom: 2px solid #28a745;
+        display: inline-block;
+        width: 100%;
+    }
+
+    .input-field img {
+        width: 7%;
+        margin-bottom: 1%;
+    }
+
+    .input-field input {
+        width: 80%;
+        border: none;
+        display: inline-block;
+    }
+
+    form {
+        background: white;
+        width: 30%;
+        margin-left: 35%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        border-radius: 15px;
+        margin-top: 10%;
+        padding: 3%;
+    }
+
+    button {
+        width: 50%;
+        margin: 5% 25%;
+    }
+
+    a {
+        width: 20%;
+        margin: 10% 40%;
+        font-size: 16px;
+        text-decoration: none;
+    }
+
+    .form-control:focus {
+        -webkit-box-shadow: none;
+        box-shadow: none;
+    }
+</style>
