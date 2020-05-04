f83b9e263ab3a7b24c02d31b7e92d3312a2dbc1f
ElinaValieva <veaufa@mail.ru>
2020-05-04 13:45:32 +0300

    Test: mock test for comment service

---
+++ src/test/java/com/quarkus/service/CommentServiceTest.java
@@ -0,0 +1,82 @@
+package com.quarkus.service;
+
+import com.quarkus.entity.CommentEntity;
+import com.quarkus.entity.CredentialEntity;
+import com.quarkus.entity.PostEntity;
+import com.quarkus.entity.UserEntity;
+import com.quarkus.exception.BusinessLogicException;
+import com.quarkus.model.Comment;
+import com.quarkus.model.UserDetail;
+import com.quarkus.repository.PostRepository;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.*;
+
+import static org.mockito.Mockito.when;
+
+@QuarkusTest
+public class CommentServiceTest {
+
+    @InjectMock
+    PostRepository postRepository;
+
+    @Inject
+    CommentService commentService;
+
+    private static final long ID = 0L;
+
+    @Test
+    void createCommentForNotExistingPost() {
+        when(postRepository.findById(ID)).thenReturn(Optional.empty());
+        Assertions.assertThrows(BusinessLogicException.class, () -> commentService.createCommentForPost("comment", ID));
+    }
+
+    @Test
+    void createCommentForExistingPost() {
+        when(postRepository.findById(ID)).thenReturn(Optional.of(PostEntity.builder().commentEntities(new ArrayList<>()).build()));
+        Assertions.assertDoesNotThrow(() -> commentService.createCommentForPost("comment", ID));
+    }
+
+    @Test
+    void getCommendsForPostForNotExistingPost() {
+        when(postRepository.findById(ID)).thenReturn(Optional.empty());
+        Assertions.assertThrows(BusinessLogicException.class, () -> commentService.getCommendsForPost(ID));
+    }
+
+    @Test
+    void getCommendsForPostForExistingPost() {
+        when(postRepository.findById(ID)).thenReturn(Optional.of(PostEntity.builder().commentEntities(getCommentEntityList()).build()));
+        Assertions.assertEquals(getCommentList(), commentService.getCommendsForPost(ID));
+    }
+
+    private List<CommentEntity> getCommentEntityList() {
+        return Collections.singletonList(CommentEntity.builder()
+                .comment("comment")
+                .date(new Date(100))
+                .id(0L)
+                .userEntity(UserEntity.builder()
+                        .name("first name")
+                        .lastName("last name")
+                        .credentialEntity(CredentialEntity.builder()
+                                .username("username")
+                                .build())
+                        .build())
+                .build());
+    }
+
+    private List<Comment> getCommentList() {
+        return Collections.singletonList(Comment.builder()
+                .commentText("comment")
+                .date("01 янв.")
+                .userDetail(UserDetail.builder()
+                        .username("username")
+                        .firstName("first name")
+                        .lastName("last name")
+                        .build())
+                .build());
+    }
+}
