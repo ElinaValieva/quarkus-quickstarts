371fa963b8246f6a07eb039308e51a6dc66d12b2
ElinaValieva <veaufa@mail.ru>
2020-05-17 13:46:36 +0300

    Fix: application imports

--- src/main/java/com/quarkus/entity/UserEntity.java
+++ src/main/java/com/quarkus/entity/UserEntity.java
@@ -1,17 +1,12 @@
 package com.quarkus.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cascade;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Table(name = "user")
--- src/main/java/com/quarkus/model/UserDetail.java
+++ src/main/java/com/quarkus/model/UserDetail.java
@@ -1,6 +1,9 @@
 package com.quarkus.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
